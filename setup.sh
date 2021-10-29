a86aaee0a4eef46335a0faf6c97229de99772724
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2021-10-29 11:22:28 +0200

    Add Install Conda and TexLive

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-packages=()
-export TEXLIVE_INSTALL_PREFIX=${HOME}/.local/texlive
-
 msg () {
     echo -e "${*}" >/dev/stderr
 }
@@ -17,8 +14,6 @@ err () {
 todo () {
     msg "=== To Do ===
 Install Packages
-Install TexLive
-Install Conda (+Mamba)
 Install Fonts
 
 Setup Configurations
@@ -32,8 +27,44 @@ Args:
         Show this help.
 "
 
+install_texlive () {
+    TEXLIVE_PREFIX="${HOME}/.local/texlive"
+    cd "$(mktemp -d)" || exit 1
+    pwd
+    curl -L http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz | tar xz
+
+    cd install-tl-* || exit 1
+
+    # empty profile uses defaults
+    touch texlive.profile
+
+    TEXLIVE_INSTALL_PREFIX=${TEXLIVE_PREFIX} \
+    TEXLIVE_INSTALL_NO_RESUME=1 \
+    TEXLIVE_INSTALL_NO_WELCOME=1 \
+    TEXLIVE_INSTALL_TEXMFHOME=~/.texmf \
+        ./install-tl -profile texlive.profile
+
+    cd "$(find "${TEXLIVE_PREFIX}" -name '20*' | sort | head -1)/bin/x86_64-linux" || exit 1
+    ./tlmgr option autobackup -- -1
+    ./tlmgr option repository http://mirror.ctan.org/systems/texlive/tlnet
+
+    warn "Setup TexLive PATH"
+}
+
+install_conda () {
+    cd "$(mktemp -d)" || exit 1
+    conda_prefix="${HOME}/.local/miniconda3"
+    filename="Miniconda3-latest-Linux-x86_64.sh"
+    curl -fsSLO "https://repo.anaconda.com/miniconda/${filename}"
+    bash "${filename}" -p "${conda_prefix}" -b -u
+    . "${conda_prefix}/etc/profile.d/conda.sh"
+    conda install -c conda-forge mamba --yes
+}
+
 case $1 in
     -h|--help) msg "${usage}" && exit ;;
+    conda) install_conda ;;
+    texlive) install_texlive ;;
     *) err "No command provided." ;;
 esac
 
