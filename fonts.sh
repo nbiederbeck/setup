d7cfc78f4703641ae1076c6645ba1cfa1083f785
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-02-15 15:27:02 +0100

    Add pre-commit and run it

--- fonts.sh
+++ fonts.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 fonts=(
-noto-fonts-emoji
-otf-latin-modern
-otf-latinmodern-math
-ttf-fira-code
-ttf-ibm-plex
-adobe-source-code-pro-fonts
-adobe-source-sans-fonts
-adobe-source-serif-fonts
+	noto-fonts-emoji
+	otf-latin-modern
+	otf-latinmodern-math
+	ttf-fira-code
+	ttf-ibm-plex
+	adobe-source-code-pro-fonts
+	adobe-source-sans-fonts
+	adobe-source-serif-fonts
 )
 
 yay -Syu --needed "${fonts[@]}"
